58096bc5bc50ed044924921b0714eb83894333af
thanh-tran <tranvanthanh.trt@gmail.com>
2023-06-19 10:53:12 +0700

    confirm popup

--- codes/RetailProject/static/order-app/js/placeOrders.js
+++ codes/RetailProject/static/order-app/js/placeOrders.js
@@ -1,9 +1,19 @@
 const popupOrderDetail = () => {
   $('#order_detail_popup').modal({
     open: true,
-    fadeDuration: 200,
+    fadeDuration: 100,
     showClose: false,
     escapeClose: true,
     clickClose: true,
+  })
+}
+const confirmCancelOrder = () => {
+  $('#confirm_cancel_order_popup').modal({
+    open: true,
+    fadeDuration: 100,
+    showClose: false,
+    escapeClose: true,
+    clickClose: true,
+    // closeExisting: false
   })
 }
