5a22a320ceb3fdbf56a77cbf5d8b820a934fd6cc
thanh-tran <tranvanthanh.trt@gmail.com>
2023-07-19 09:22:35 +0700

    remove unuse code

--- codes/RetailProject/RetailProject/asgi.py
+++ codes/RetailProject/RetailProject/asgi.py
@@ -14,6 +14,6 @@ from django.core.asgi import get_asgi_application
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RetailProject.settings')
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(BASE_DIR, 'retail-project-148d8-firebase-adminsdk-9sn3z-8b15a12ccd.json')
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(BASE_DIR, 'retail-project-148d8-firebase-adminsdk-9sn3z-8b15a12ccd.json')
 
 application = get_asgi_application()
--- codes/RetailProject/RetailProject/wsgi.py
+++ codes/RetailProject/RetailProject/wsgi.py
@@ -14,5 +14,5 @@ from django.core.wsgi import get_wsgi_application
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RetailProject.settings')
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(BASE_DIR, 'retail-project-148d8-firebase-adminsdk-9sn3z-8b15a12ccd.json')
+# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(BASE_DIR, 'retail-project-148d8-firebase-adminsdk-9sn3z-8b15a12ccd.json')
 application = get_wsgi_application()
